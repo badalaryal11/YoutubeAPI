b12a39ac692266d77997793a5f78a64411d8cead
Badal Aryal <badal.aryal@outlook.com>
2022-03-27 10:29:29 -0700

    getting youtube data back sucessfully and diplkaying in our webapp

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,9 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using YoutubeAPI.Models;
+using Youtube_API.Models;
 
-namespace YoutubeAPI.Controllers
+namespace Youtube_API.Controllers
 {
     public class HomeController : Controller
     {
--- Models/YoutubeSearch.cs
+++ Models/YoutubeSearch.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace YoutubeAPI.Models
+namespace Youtube_API.Models
 {
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
     public class PageInfo
